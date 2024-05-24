e3ece25d42867f84bf2c1ef5547627b302281ef1
xiaoyan <18534701642@163.com>
2024-05-24 21:48:26 +0800

    提交

--- Assets/Scripts/TestTest.cs
+++ Assets/Scripts/TestTest.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class TestTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("测试");
+        Debug.Log("测试Test");
     }
 
     // Update is called once per frame
