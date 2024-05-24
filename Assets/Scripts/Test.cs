0df3d788015fb0b91ecbb9ef021e475ba52a1fcf
xiaoyan <18534701642@163.com>
2024-05-24 20:15:01 +0800

    备注

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("²âÊÔ");
+        Debug.Log("æµ‹è¯•");
     }
 
     // Update is called once per frame
